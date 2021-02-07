0041ecb623d11f76d2015e2bd2d1fec75f70aab1
ArthurMarianoNeto <thiesto@gmail.com>
2021-02-06 21:13:12 -0300

    Alteração no app build.gradle minSdkVersion 16 para 21

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,10 +9,13 @@ class MyApp extends StatelessWidget {
    @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Loja Kabulosa',
       theme: ThemeData(
         primarySwatch: Colors.lightBlue,
+        primaryColor: Color.fromARGB(255, 4, 125, 141)
       ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
