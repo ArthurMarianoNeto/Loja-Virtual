887931c635875122f4b4360e63463c419f55de4a
ArthurMarianoNeto <thiesto@gmail.com>
2021-02-10 09:45:51 -0300

    Incluindo Drawer lateral incluindo estilização

---
+++ lib/widgets/custom_drawer.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart ';
+
+class CustomDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    Widget _buildDrawerBack() => Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 203, 236, 241),
+                Colors.white,
+              ],
+              begin: Alignment.topCenter,
+              end:  Alignment.bottomCenter
+          )
+      ),
+    );
+
+    return Drawer(
+      child: Stack(
+        children: [
+          _buildDrawerBack(),
+          ListView(
+            padding: EdgeInsets.only(left: 32, top: 16),
+          )
+        ],
+      ),
+    );
+  }
+}
