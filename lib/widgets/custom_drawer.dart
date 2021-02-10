a402cfcb450bbed9efaaf39b104ab7ad5b577916
ArthurMarianoNeto <thiesto@gmail.com>
2021-02-10 11:03:05 -0300

    Inclusão de Opçoes e icones no custom_drawer.dart e estilização

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart ';
+import 'package:loja_virtual/tiles/drawer_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
   @override
@@ -36,10 +37,42 @@ class CustomDrawer extends StatelessWidget {
                         child: Text("Mega Moda209", // Text("Mega \nModa209",
                           style: TextStyle(fontSize: 34.0, fontWeight: FontWeight.bold),
                         )
+                    ),
+                    Positioned(
+                        left: 0.0,
+                        bottom: 0.0,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Seja bem Vindo!",
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold
+                              ),
+                            ),
+                           GestureDetector(
+                             child:  Text("Faça Login ou Cadastre-se ->",
+                               style: TextStyle(
+                                   color: Theme.of(context).primaryColor,
+                                   fontSize: 16.0,
+                                   fontWeight: FontWeight.bold
+                               ),
+                             ),
+                             onTap: (){
+
+                             },
+                           )
+                          ],
+                        ),
                     )
                   ],
                 ),
-              )
+              ),
+              Divider(),
+              DrawerTile(Icons.home, "Início"),
+              DrawerTile(Icons.list, "Produtos"),
+              DrawerTile(Icons.location_on, "Lojas"),
+              DrawerTile(Icons.playlist_add_check, "Meus Pedidos"),
             ],
           )
         ],
